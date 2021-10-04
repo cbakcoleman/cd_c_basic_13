fcff4ba7a233dac6bd068f891a837ac4087815ec
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-10-04 13:25:40 -0700

    submit homework assignment

--- Program.cs
+++ Program.cs
@@ -66,6 +66,10 @@ namespace cd_c_basic_13
         {
             // Write a function that would iterate through each item of the given integer array and 
             // print each value to the console. 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                System.Console.WriteLine(numbers[i]);
+            }
         }
 
         public static int FindMax(int[] numbers)
@@ -73,18 +77,44 @@ namespace cd_c_basic_13
             // Write a function that takes an integer array and prints and returns the maximum value in the array. 
             // Your program should also work with a given array that has all negative numbers (e.g. [-3, -5, -7]), 
             // or even a mix of positive numbers, negative numbers and zero.
+            int maxNum = 0;
+            int idx = 0;
+            for (int i = 0; i<numbers.Length; i++)
+            {
+                if (numbers[idx] > maxNum)
+                {
+                    maxNum = numbers[i ]; 
+                }
+            }
+            return maxNum;
         }
 
         public static void GetAverage(int[] numbers)
         {
             // Write a function that takes an integer array and prints the AVERAGE of the values in the array.
             // For example, with an array [2, 10, 3], your program should write 5 to the console.
+            int sum = 0;
+            for (int i = 0; i <= numbers.Length; i++)
+            {
+                sum = sum + numbers[i];
+            }
+            System.Console.WriteLine(sum / numbers.Length);
         }
 
         public static int[] OddArray()
         {
             // Write a function that creates, and then returns, an array that contains all the odd numbers between 1 to 255. 
             // When the program is done, this array should have the values of [1, 3, 5, 7, ... 255].
+            int[] newArr;
+            for (int i = 1; i <= 255; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    newArr = new int[] {i};
+                }
+                return newArr;
+            }
+            return null;
         }
 
         public static int GreaterThanY(int[] numbers, int y)
@@ -93,24 +123,54 @@ namespace cd_c_basic_13
             // That are greater than the "y" value. 
             // For example, if array = [1, 3, 5, 7] and y = 3. Your function should return 2 
             // (since there are two values in the array that are greater than 3).
+            int greaterThan = 0;
+            for (int i = 0; i <= numbers.Length; i++)
+            {
+                if (numbers[i] > y)
+                {
+                    greaterThan++;
+                }
+            }
+            return greaterThan;
         }
 
         public static void SquareArrayValues(int[] numbers)
         {
             // Write a function that takes an integer array "numbers", and then multiplies each value by itself.
             // For example, [1,5,10,-10] should become [1,25,100,100]
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = numbers[i] * numbers[i];
+            }
+            System.Console.WriteLine(numbers);
         }
 
         public static void EliminateNegatives(int[] numbers)
         {
             // Given an integer array "numbers", say [1, 5, 10, -2], create a function that replaces any negative number with the value of 0. 
             // When the program is done, "numbers" should have no negative values, say [1, 5, 10, 0].
+            for (int i = 0; i < numbers.Length; i++)
+            if (numbers[i] < 0)
+            {
+                numbers[i] = 0;
+            }
+            System.Console.WriteLine(numbers);
         }
 
         public static void MinMaxAverage(int[] numbers)
         {
             // Given an integer array, say [1, 5, 10, -2], create a function that prints the maximum number in the array, 
             // the minimum value in the array, and the average of the values in the array.
+            int idx = 0;
+            int minNum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] <= numbers[idx])
+                {
+                    minNum = numbers[i];
+                }
+            }
+
         }
 
         public static void ShiftValues(int[] numbers)
@@ -119,6 +179,14 @@ namespace cd_c_basic_13
             // Write a function that shifts each number by one to the front and adds '0' to the end. 
             // For example, when the program is done, if the array [1, 5, 10, 7, -2] is passed to the function, 
             // it should become [5, 10, 7, -2, 0].
+            int tempshift = 0;
+            for (int i = 0; i <= numbers.Length; i++)
+            {
+                tempshift = numbers[i + 1];
+                numbers[i] = tempshift;
+            }
+            numbers[numbers.Length -1] = 0;
+            System.Console.WriteLine(numbers);
         }
 
         public static object[] NumToString(int[] numbers)
@@ -127,6 +195,7 @@ namespace cd_c_basic_13
             // that replaces any negative number with the string 'Dojo'.
             // For example, if array "numbers" is initially [-1, -3, 2] 
             // your function should return an array with values ['Dojo', 'Dojo', 2].
+            
         }
     }
 }
